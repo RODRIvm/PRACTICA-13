c7baade8efd51777c9c24d5a18c30356c922be79
CASA <CASA@192.168.1.77>
2022-05-24 07:54:37 -0500

    14

---
+++ src/rodri/rodri.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package rodri;
+
+/**
+ *
+ * @author rodri
+ */
+public class rodri {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFPrincipal jFPrincipal = new JFPrincipal();
+        jFPrincipal.setVisible(true);
+    }
+    
+}
